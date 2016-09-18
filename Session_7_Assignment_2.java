cf3c92209f99b7b9e77a4f18c52a5878bf2f6a26
rbrahul95 <rbrahul95@gmail.com>
2016-09-18 21:58:05 +0530

    Add files via upload

---
+++ Session_7_Assignment_2.java
@@ -0,0 +1,31 @@
+/*Assignment – Create student result processing system with an abstract
+class.
+*/
+
+public class Session_7_Assignment_2 {
+ public static void main(String[] args) {
+		
+		
+		try{
+			
+			 int num=Integer.parseInt ("XYZ") ;
+			 System.out.println(num);
+		}
+		catch(Exception e){
+			
+			System.out.println(e);
+			
+		}
+		
+	}
+	
+}
+
+/* 
+  Output :   
+  
+
+  java.lang.NumberFormatException: For input string: "XYZ"
+  
+ 
+*/
